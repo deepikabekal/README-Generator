96c88645e00f1b81a9f593810f5ee312839b4ae0
Deepika Bekal <deepika.bekal2@gmail.com>
2021-04-22 20:56:56 +0400

    include packages and create array of questions

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require("inquirer");
+const fs = require("fs");
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    "Enter application name.", 
+    "Enter description for the application",
+    "Enter table of contents.",
+    "Enter instructions to install the application.",
+    "Enter Usage information",
+    "Choose License for your application.",
+    "Enter contribution guidelines.",
+    "Enter test instructions.", 
+    "Enter GitHub username.",
+    "Enter email address."
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
