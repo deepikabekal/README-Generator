7d7a601241cd22704ee4c7b734763826c6409436
Deepika Bekal <deepika.bekal2@gmail.com>
2021-04-22 22:30:03 +0400

    prompt questions and capture user input

--- index.js
+++ index.js
@@ -3,23 +3,65 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 // TODO: Create an array of questions for user input
 const questions = [
-    "Enter application name.", 
-    "Enter description for the application",
-    "Enter table of contents.",
-    "Enter instructions to install the application.",
-    "Enter Usage information",
-    "Choose License for your application.",
-    "Enter contribution guidelines.",
-    "Enter test instructions.", 
-    "Enter GitHub username.",
-    "Enter email address."
+    {
+        type : "input",
+        name : "title",
+        message : "Enter application name."
+    },
+    {
+        type : "input",
+        name : "description",
+        message : "Enter description for the application"
+    },
+    {
+        type : "input",
+        name : "instructions",
+        message : "Enter instructions to install the application."
+
+    },
+    {
+        type : "input",
+        name : "usage",
+        message : "Enter Usage information"
+    },
+    {
+        type : "checkbox",
+        name : "license",
+        message : "Choose License for your application.",
+        choices : ["Apache 2.0 License", "The MIT License", "Mozilla Public License 2.0", "Open Database License (ODbL)", "Public Domain Dedication and License (PDDL)"]
+    },
+    {
+       type : "input",
+       name : "contributing",
+       message : "Enter contribution guidelines."
+    },
+    {
+        type : "input",
+        name : "test",
+        message : "Enter test instructions."
+    },
+    {
+        type : "input",
+        name : "github",
+        message : "Enter GitHub username."
+    },
+    {
+        type : "input",
+        name : "email",
+        message : "Enter email address." 
+    }
 ];
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init () {
+    return inquirer.prompt(questions);
+}
+
 
 // Function call to initialize app
-init();
+init()
+.then (data => console.log(data));
